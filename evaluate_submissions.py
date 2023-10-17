065c351ca041a28489c7e1c70edf5451d7f0ffe7
Jonas Vestby <jonves@prio.org>
2023-10-17 15:14:31 +0200

    refactor to fit with apache hive data structure

--- evaluate_submissions.py
+++ evaluate_submissions.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import yaml
 from CompetitionEvaluation import load_data, structure_data, calculate_metrics
+from utilities import list_submissions, get_predictions, TargetType
 from dataclasses import dataclass
 import os
 import xarray
@@ -8,100 +9,38 @@ import numpy as np
 import numpy.typing as npt
 from scipy.signal import resample
 import argparse
+import pandas as pd
+import pyarrow
 
 import logging
 logging.getLogger(__name__)
 logging.basicConfig(filename='evaluate_submission.log', encoding='utf-8', level=logging.INFO)
 
-def evaluate_forecast(forecast_file: str | os.PathLike, expected_samples: int, actuals_folder: str|os.PathLike, submission_root: str|os.PathLike) -> None:
-
-    @dataclass(frozen=True)
-    class Actuals:
-        target: str
-        test_window: str
-        parquet_file: str
-
-    actuals = [Actuals("cm", "test_window_2018", "cm_actuals_2018.parquet"),
-    Actuals("cm", "test_window_2019", "cm_actuals_2019.parquet"),
-    Actuals("cm", "test_window_2020", "cm_actuals_2020.parquet"),
-    Actuals("cm", "test_window_2021", "cm_actuals_2021.parquet"),
-    Actuals("pgm", "test_window_2018", "pgm_actuals_2018.parquet"),
-    Actuals("pgm", "test_window_2019", "pgm_actuals_2019.parquet"),
-    Actuals("pgm", "test_window_2020", "pgm_actuals_2020.parquet"),
-    Actuals("pgm", "test_window_2021", "pgm_actuals_2021.parquet")]
-
-    _, name, target, window = forecast_file.relative_to(submission_root).parent.parts
-    actual = [actual for actual in actuals if actual.target == target and actual.test_window == window]
-
-    if len(actual) != 1:
-        raise ValueError("Only one hit allowed.")
-    actual = actual[0]
-    actual_file = actuals_folder/actual.target/actual.test_window/actual.parquet_file
+def evaluate_forecast(forecast: pd.DataFrame, 
+                      actuals: pd.DataFrame, 
+                      target: TargetType,
+                      expected_samples: int,
+                      save_to: str|os.PathLike,
+                      draw_column_name: str ="draw", 
+                      data_column_name: str = "outcome",
+                      bins: list[float] = [0, 0.5, 2.5, 5.5, 10.5, 25.5, 50.5, 100.5, 250.5, 500.5, 1000.5]) -> None:
 
     if target == "pgm":
-        target_column = "priogrid_gid"
+        unit = "priogrid_gid"
     elif target == "cm":
-        target_column = "country_id"
+        unit = "country_id"
     else:
         raise ValueError(f'Target {target} must be either "pgm" or "cm".')
 
-    observed, predictions = load_data(observed_path = actual_file, forecasts_path=forecast_file)
-
-    if predictions.index.names != ['month_id', target_column, 'draw']:
-        if predictions.index.names == [None] and all([var in predictions.columns for var in ['month_id', target_column, 'draw']]):
-            # Index is not set, but index variables are in the file.
-            predictions[target_column] = predictions[target_column].astype(int)
-            predictions["month_id"] = predictions["month_id"].astype(int)
-            predictions.set_index(['month_id', target_column, 'draw'], inplace = True)
-        else: 
-            logging.warning(f'Predictions file {forecast_file} does not have correct index. Currently: {predictions.index.names}')
-            if len(predictions.index.names) == 3:
-                logging.warning(f'Attempts to rename index.')
-                predictions.index.names = ['month_id', target_column, 'draw']
-            else:
-                raise ValueError(f'Predictions file {forecast_file} does not contain correct index columns.')
-
-    if len(observed.columns) == 1 and "outcome" not in observed.columns:
-        logging.warning(f'Actuals file {actual_file} does not have the "outcome" folder.')
-        logging.warning(f'Renaming column.')
-        observed.columns = ["outcome"]
-
-    if len(predictions.columns) == 1 and "outcome" not in observed.columns:
-        logging.warning(f'Predictions file {forecast_file} does not have the "outcome" folder.')
-        logging.warning(f'Renaming column.')
-        predictions.columns = ["outcome"]
-
-    if len(predictions.columns) != 1:
-        raise ValueError("Predictions file can only have 1 column.")
-
-    if len(observed.columns) != 1:
-        raise ValueError("Actuals file can only have 1 column.")
-    
-    #units_in_predictions_not_in_observed = [c for c in predictions.index.unique(level=target_column) if c not in observed.index.unique(level=target_column)]
-    #units_in_observed_not_in_predictions = [c for c in observed.index.unique(level=target_column) if c not in predictions.index.unique(level=target_column)]
-
-    #times_in_observed_not_in_predictions = [c for c in observed.index.unique(level="month_id") if c not in predictions.index.unique(level="month_id")]
-    #times_in_predictions_not_in_observed = [c for c in predictions.index.unique(level="month_id") if c not in observed.index.unique(level="month_id")]
-
-    #assert len(units_in_predictions_not_in_observed) == 0, f'Lacking actuals for {target_column}: {units_in_predictions_not_in_observed}'
-    #assert len(units_in_observed_not_in_predictions) == 0, f'Lacking predictions for {target_column}: {units_in_observed_not_in_predictions}'
-    #assert len(times_in_observed_not_in_predictions) == 0, f'Lacking predictions for {target_column}: {times_in_observed_not_in_predictions}'
-    #assert len(times_in_predictions_not_in_observed) == 0, f'Lacking actuals for {target_column}: {times_in_predictions_not_in_observed}'
-
-    observed, predictions = structure_data(observed, predictions, draw_column_name="draw", data_column_name = "outcome")
-
-    predictions[target_column] in observed[target_column]
-    predictions["month_id"] in observed["month_id"]
+    # Cast to xarray
+    observed, predictions = structure_data(actuals, forecast, draw_column_name=draw_column_name, data_column_name = data_column_name)
 
     if bool((predictions["outcome"] > 10e9).any()):
         logging.warning(f'Found predictions larger than earth population. These are censored at 10 billion.')
         predictions["outcome"] = xarray.where(predictions["outcome"]>10e9, 10e9, predictions["outcome"])
         
-    crps_per_unit = calculate_metrics(observed, predictions, metric = "crps", aggregate_over="month_id")
-    mis_per_unit = calculate_metrics(observed, predictions, metric = "mis", prediction_interval_level = 0.9, aggregate_over="month_id")
-
-    crps_per_month = calculate_metrics(observed, predictions, metric = "crps", aggregate_over=target_column)
-    mis_per_month = calculate_metrics(observed, predictions, metric = "mis", prediction_interval_level = 0.9, aggregate_over=target_column)
+    crps = calculate_metrics(observed, predictions, metric = "crps", aggregate_over="nothing")
+    mis = calculate_metrics(observed, predictions, metric = "mis", prediction_interval_level = 0.9, aggregate_over="nothing")
 
     if predictions.dims['member'] != expected_samples:
         logging.warning(f'Number of samples ({predictions.dims["member"]}) is not 1000. Using scipy.signal.resample to get {expected_samples} samples when calculating Ignorance Score.')
@@ -110,7 +49,7 @@ def evaluate_forecast(forecast_file: str | os.PathLike, expected_samples: int, a
         arr = np.where(arr<0, 0, arr) # For the time when resampling happens to go below zero.
 
         new_container = predictions.sel(member = 1)
-        new_container = new_container.expand_dims({"member": range(0,expected_samples)}).to_array().transpose("variable", "month_id", target_column, "member")
+        new_container = new_container.expand_dims({"member": range(0,expected_samples)}).to_array().transpose("variable", "month_id", unit, "member")
         predictions: xarray.Dataset = xarray.DataArray(data = arr, coords = new_container.coords).to_dataset(dim="variable")
 
     if bool((predictions["outcome"] < 0).any()):
@@ -118,54 +57,75 @@ def evaluate_forecast(forecast_file: str | os.PathLike, expected_samples: int, a
         predictions["outcome"] = xarray.where(predictions["outcome"]<0, 0, predictions["outcome"])
 
 
-    ign_per_unit = calculate_metrics(observed, predictions, metric = "ign", bins = [0, 0.5, 2.5, 5.5, 10.5, 25.5, 50.5, 100.5, 250.5, 500.5, 1000.5], aggregate_over="month_id")
-    ign_per_month = calculate_metrics(observed, predictions, metric = "ign", bins = [0, 0.5, 2.5, 5.5, 10.5, 25.5, 50.5, 100.5, 250.5, 500.5, 1000.5], aggregate_over=target_column)
+    ign = calculate_metrics(observed, predictions, metric = "ign", bins = bins, aggregate_over="nothing")
     
-    eval_path = forecast_file.parent/"eval"
-    eval_path.mkdir(exist_ok=True)
-    crps_per_unit.to_parquet(eval_path/"crps_per_unit.parquet")
-    crps_per_month.to_parquet(eval_path/"crps_per_month.parquet")
-    ign_per_unit.to_parquet(eval_path/"ign_per_unit.parquet")
-    ign_per_month.to_parquet(eval_path/"ign_per_month.parquet")
-    mis_per_unit.to_parquet(eval_path/"mis_per_unit.parquet")
-    mis_per_month.to_parquet(eval_path/"mis_per_month.parquet")
+    # Long-format out
+    crps.rename(columns={"ign": "value"}, inplace=True)
+    ign.rename(columns={"ign": "value"}, inplace=True)
+    mis.rename(columns={"ign": "value"}, inplace=True)
+    
+    (save_to / "metric=crps").mkdir(exist_ok=True, parents = True)
+    (save_to / "metric=ign").mkdir(exist_ok=True, parents = True)
+    (save_to / "metric=mis").mkdir(exist_ok=True, parents = True)
+    crps.to_parquet(save_to / "metric=crps" / "crps.parquet")
+    ign.to_parquet(save_to / "metric=ign" / "ign.parquet")
+    mis.to_parquet(save_to / "metric=mis" / "mis.parquet")
+
+def match_forecast_with_actuals(submission, actuals_folder, target: TargetType, year):
+
+    filter = pyarrow.compute.field("year") == year
+    actuals = get_predictions(actuals_folder, target = target, filters = filter)
+    predictions = get_predictions(submission, target = target, filters = filter)
+
+    predictions.drop(columns=["year"], inplace = True)
+    actuals.drop(columns=["year"], inplace = True)
+
+    return actuals, predictions
 
 def main():
     parser = argparse.ArgumentParser(description="Method for evaluation of submissions to the ViEWS Prediction Challenge",
                                      epilog = "Example usage: python evaluate_submissions.py -s ./submissions -a ./actuals -e 100")
     parser.add_argument('-s', metavar='submissions', type=str, help='path to folder with submissions complying with submission_template')
     parser.add_argument('-a', metavar='actuals', type=str, help='path to folder with actuals')
+    parser.add_argument('-t', metavar='targets', nargs = "+", type=str, help='pgm or cm or both', default = ["pgm", "cm"])
+    parser.add_argument('-y', metavar='years', nargs = "+", type=int, help='yearly windows to evaluate', default = [2018, 2019, 2020, 2021])
     parser.add_argument('-e', metavar='expected', type=int, help='expected samples', default = 1000)
+    parser.add_argument('-sc', metavar='sample-column-name', type=str, help='(Optional) name of column for the unique samples', default = "draw")
+    parser.add_argument('-dc', metavar='data-column-name', type=str, help='(Optional) name of column with data, must be same in both observed and predictions data', default = "outcome")
+    parser.add_argument('-ib', metavar = 'ign-bins', nargs = "+", type = float, help='Set a binning scheme for the ignorance score. List or integer (nbins). E.g., "--ib 0 0.5 1 5 10 100 1000". None also allowed.', default = [0, 0.5, 2.5, 5.5, 10.5, 25.5, 50.5, 100.5, 250.5, 500.5, 1000.5])    
+
     args = parser.parse_args()
 
 
     submission_path = Path(args.s)
     actuals_folder = Path(args.a)
     expected_samples = args.e
+    targets = args.t
+    years = args.y
+    sample_column_name = args.sc
+    data_column_name = args.dc
+    bins = args.ib
 
-    submission_root = submission_path.parent
-
-    submissions = [submission for submission in submission_path.iterdir() if submission.is_dir() and not submission.stem == "__MACOSX"]
+    submissions = list_submissions(submission_path)
 
     for submission in submissions:
-
         try:
-            with open(submission/"submission_details.yml") as f:
-                submission_details = yaml.safe_load(f)
-        except:
-            logging.error(f'{submission/"submission_details.yml"} could not be loaded.')
-
-        prediction_files = list(submission.glob("**/*.parquet"))
-        prediction_files = [f for f in prediction_files if not f.stem.split("_")[0] == "eval"]
-        prediction_files = [f for f in prediction_files if not f.parent.parts[-1] == "eval"]
-        prediction_files = [f for f in prediction_files if "__MACOSX" not in f.parts]
-        
-        for f in prediction_files:
-            try:
-                logging.info(f'Evaluating {str(f)}')
-                evaluate_forecast(f, expected_samples, actuals_folder, submission_root)
-            except Exception as e:
-                logging.error(f'{str(e)}')
+            logging.info(f'Evaluating {submission.name}')
+            for target in targets:
+                for year in years:
+                    if any((submission / target).glob("**/*.parquet")): # test if there are prediction files in the target
+                        actuals, predictions = match_forecast_with_actuals(submission, actuals_folder, target, year)
+                        save_to = submission / "eval" / f'target={target}' / f'year={year}'
+                        evaluate_forecast(forecast = predictions, 
+                                        actuals = actuals, 
+                                        target = target,
+                                        expected_samples = expected_samples,
+                                        draw_column_name=sample_column_name,
+                                        data_column_name=data_column_name,
+                                        bins = bins,
+                                        save_to = save_to)
+        except Exception as e:
+            logging.error(f'{str(e)}')
 
 
 if __name__ == "__main__":
