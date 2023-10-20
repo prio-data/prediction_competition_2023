688b37e73b0e868190c7484b55972c11f9ef3e2e
Jonas Vestby <jonves@prio.org>
2023-10-20 09:46:59 +0200

    delete eval when migrating

--- utilities.py
+++ utilities.py
@@ -45,23 +45,17 @@ def migrate_submission_from_old(submission: str|os.PathLike) -> None:
     submission = Path(submission)
     eval_data = itertools.chain(submission.glob(f'**/cm/*/eval/*.parquet'),
                                 submission.glob(f'**/pgm/*/eval/*.parquet'))
-    def file_rename(f):
-        target, window = f.parent.parts[-3:-1]
-        eval_folder_path = submission / "eval" / target / window
-        eval_folder_path.mkdir(parents=True, exist_ok=True)
-        f.rename(eval_folder_path / f.name)
-    
+        
     def folder_rename(d):
-        new_name = d.parent / f'year={d.name.split("_")[-1]}'
+        new_name = d.parent / f'window=Y{d.name.split("_")[-1]}'
         d.rename(new_name)
 
     # Rename window folders in Apache Hive format
     window_folders = itertools.chain(submission.glob(f'cm/test_window_*'), submission.glob(f'pgm/test_window_*'))
     [folder_rename(d) for d in window_folders]
-    # Move evaluation files into eval folder
-    [file_rename(f) for f in eval_data]        
-    # Move summary files into eval folder
-    [f.rename(submission / "eval" / f.name) for f in submission.glob(f'eval*.parquet')]
+    # Delete evaluation files (must be re-estimated)
+    [f.unlink() for f in eval_data]        
+    [f.unlink() for f in submission.glob(f'eval*.parquet')]
     
     # Cleanup old folders (only deletes if empty)
     [d.rmdir() for d in submission.glob(f'**/cm/*/eval/')]
@@ -70,7 +64,7 @@ def migrate_submission_from_old(submission: str|os.PathLike) -> None:
 def list_submissions(submissions_folder: str|os.PathLike) -> list[os.PathLike]:
     submissions_folder = Path(submissions_folder)
     return [submission for submission in submissions_folder.iterdir() if submission.is_dir() and not submission.stem == "__MACOSX"]
-
+    
 def read_parquet(data_path: str|os.PathLike, filters = None) -> pd.DataFrame:
     """
     This function does not need to be used directly, it is mostly to document how to read folders with parquet files or single parquet files with a filter.
@@ -113,3 +107,6 @@ def get_predictions(submission: str|os.PathLike, target: TargetType, filters = N
     """
     submission = Path(submission)
     return read_parquet(submission / target, filters = filters)
+
+def get_window_filters(window):
+    pass
