e38c6efcfabc7409825a4c299bda107970ee49ee
xiaolongsun <95378566+xiaolong0728@users.noreply.github.com>
2025-05-21 12:54:04 +0200

    add yearly plot for metrics

--- plotting/plot.py
+++ plotting/plot.py
@@ -46,15 +46,17 @@ def setup_map_plot(target: str) -> Tuple[plt.Figure, plt.Axes, ccrs.Projection]:
         crs = ccrs.EqualEarth()
     else:
         crs = ccrs.PlateCarree()
-    
+
     fig, ax = plt.subplots(figsize=(24, 8), subplot_kw={"projection": crs})
     return fig, ax, crs
 
 
 def add_views_logo(fig: plt.Figure, target: str) -> None:
-    logo_url = "https://cdn.cloud.prio.org/images/c784369fb4ae42acb7ee882e91056d92.png?x=800&"
+    logo_url = (
+        "https://cdn.cloud.prio.org/images/c784369fb4ae42acb7ee882e91056d92.png?x=800&"
+    )
     response = requests.get(logo_url, stream=True)
-    
+
     if response.status_code == 200:
         logo_img = Image.open(response.raw)
         left = 0.2 if target == "cm" else 0.4
@@ -63,58 +65,83 @@ def add_views_logo(fig: plt.Figure, target: str) -> None:
         logo_ax.axis("off")
 
 
-def format_title(team: str, model: str, window: str, month: Optional[str] = None) -> str:
+def format_title(
+    team: str, model: str, window: str, month: Optional[str] = None
+) -> str:
     formatted_model = model.replace(team, "").strip().lstrip("_")
     formatted_team = team.replace("_", " ").strip().title()
     formatted_year = int(window)
-    
+
     if month:
         return f"Team: {formatted_team}, Model: {formatted_model}, Window: {formatted_year}, Month: {month}"
     return f"Team: {formatted_team}, Model: {formatted_model}, Window: {formatted_year}"
 
 
-def prepare_geo_forecast_data(submission: Path, target: str, window: str | int, month: str | int) -> Tuple[gpd.GeoDataFrame, pd.DataFrame, str, str]:
+def prepare_geo_forecast_data(
+    submission: Path, target: str, window: str | int, month: str | int
+) -> Tuple[gpd.GeoDataFrame, pd.DataFrame, str, str]:
     unit = get_unit_column(target)
     shapefile_path = get_shapefile_path(target)
     team, model = load_submission_details(submission)
-    
+
     map = gpd.read_file(shapefile_path)
     if target == "pgm":
         map = map.rename(columns={"priogrid_i": "priogrid_gid"})
-    
+
     outcome_path = submission / f"{target}" / f"window=Y{window}"
     outcome_file = list(outcome_path.glob("**/*.parquet"))[0]
-    
+
     outcome_df = pq.read_table(outcome_file).to_pandas()
 
     if month:
-        outcome_df = outcome_df.groupby(["month_id", unit])["outcome"].median().reset_index()
+        outcome_df = (
+            outcome_df.groupby(["month_id", unit])["outcome"].median().reset_index()
+        )
 
         if month not in outcome_df.month_id.unique():
             raise ValueError(f"Month {month} not found.")
-        
-        df = pd.merge(outcome_df, map, left_on=unit, right_on=unit).query(f"month_id == {month}")
-    
+
+        df = pd.merge(outcome_df, map, left_on=unit, right_on=unit).query(
+            f"month_id == {month}"
+        )
+
     else:
-        outcome_df = outcome_df.groupby(["month_id", unit])["outcome"].median().groupby(by=unit).sum().reset_index()
+        outcome_df = (
+            outcome_df.groupby(["month_id", unit])
+            .median()
+            .groupby(by=unit)
+            .sum()
+            .reset_index()
+        )
         df = pd.merge(outcome_df, map, left_on=unit, right_on=unit)
-    
+
     return gpd.GeoDataFrame(df), outcome_df, team, model
 
 
-def prepare_geo_evaluation_data(submission: Path, target: str, window: str, metric: str) -> Tuple[gpd.GeoDataFrame, pd.DataFrame, str, str]:
+def prepare_geo_evaluation_data(
+    submission: Path,target: str, window: str, metric: str, month: str | int,
+) -> Tuple[gpd.GeoDataFrame, pd.DataFrame, str, str]:
     unit = get_unit_column(target)
     shapefile_path = get_shapefile_path(target)
     team, model = load_submission_details(submission)
-    
+
     map = gpd.read_file(shapefile_path)
     if target == "pgm":
         map = map.rename(columns={"priogrid_i": "priogrid_gid"})
-    
+
     eval_file = submission / "eval" / f"{target}" / f"window=Y{window}" / f"metric={metric}" / f"{metric}.parquet"
-    eval_df = pq.read_table(eval_file).to_pandas().reset_index()
-    
-    df = pd.merge(eval_df, map, left_on=unit, right_on=unit)
+    eval_df = pq.read_table(eval_file).to_pandas()
+
+    if month:
+        eval_df = eval_df.reset_index()
+        if month not in eval_df.month_id.unique():
+            raise ValueError(f"Month {month} not found.")
+        df = pd.merge(eval_df, map, left_on=unit, right_on=unit).query(
+            f"month_id == {month}"
+        )
+    else:
+        eval_df = eval_df.groupby(by=unit).sum().reset_index() # Not sure if the metrics should be summed
+        df = pd.merge(eval_df, map, left_on=unit, right_on=unit)
     return gpd.GeoDataFrame(df), eval_df, team, model
 
 
@@ -147,7 +174,7 @@ def choropleth_map_forecast(
     Args:
         submission: Path to the submission directory
         target: Target variable, either "cm" or "pgm"
-        window: Window size 
+        window: Window size
         month: Month to plot, if None, the sum of all months will be plotted
         cmap: Colormap to use
         metric_ticks: Ticks for the colorbar
@@ -156,31 +183,35 @@ def choropleth_map_forecast(
     submission = Path(submission)
     if window.startswith("Y"):
         window = int(window.replace("Y", ""))
-    
-    df, outcome_df, team, model = prepare_geo_forecast_data(submission, target, window, month)
+
+    df, outcome_df, team, model = prepare_geo_forecast_data(
+        submission, target, window, month
+    )
     fig, ax, crs = setup_map_plot(target)
-    
+
     vmin, vmax = df["outcome"].min(), df["outcome"].max()
     cbar = plt.cm.ScalarMappable(
         norm=matplotlib.colors.SymLogNorm(linthresh=10, vmin=vmin, vmax=vmax, base=10),
         cmap=cmap,
     )
-    
+
     sns.set_theme(style="white")
-    
+
     if target == "cm":
         df = df.to_crs(crs.proj4_init + " +over")
-        ax.add_geometries(df["geometry"], crs=crs, facecolor="none", edgecolor="whitesmoke")
+        ax.add_geometries(
+            df["geometry"], crs=crs, facecolor="none", edgecolor="whitesmoke"
+        )
         ax.set_global()
     else:
         cm_map = gpd.read_file(get_shapefile_path("cm"))
         df = df.to_crs(crs)
         cm_map.boundary.plot(ax=ax, linewidth=0.2)
         ax.set_extent([-20, 70, -40, 45])
-    
+
     if views_logo:
         add_views_logo(fig, target)
-    
+
     df.plot(
         ax=ax,
         column="outcome",
@@ -188,7 +219,7 @@ def choropleth_map_forecast(
         edgecolor="#FF000000",
         cmap=cmap,
     )
-    
+
     fig.colorbar(
         cbar,
         ax=ax,
@@ -196,7 +227,7 @@ def choropleth_map_forecast(
         label="Fatalities",
         ticks=metric_ticks,
     )
-    
+
     ax.gridlines(draw_labels=True, zorder=0)
     ax.set_title(
         format_title(team, model, window, month),
@@ -212,6 +243,7 @@ def choropleth_map_evaluation(
     metric: str,
     target: str,
     window: str,
+    month: Optional[str | int] = None,
     cmap: str = "viridis",
     metric_ticks: Optional[List[int]] = None,
     views_logo: bool = True,
@@ -224,7 +256,7 @@ def choropleth_map_evaluation(
         submission: Path to the submission directory
         metric: Metric to plot
         target: Target variable, either "cm" or "pgm"
-        window: Window size 
+        window: Window size
         cmap: Colormap to use
         metric_ticks: Ticks for the colorbar
         views_logo: Whether to add the Views logo to the plot
@@ -233,36 +265,42 @@ def choropleth_map_evaluation(
     submission = Path(submission)
     if window.startswith("Y"):
         window = int(window.replace("Y", ""))
-    
-    df, eval_df, team, model = prepare_geo_evaluation_data(submission, target, window, metric)
+
+    df, eval_df, team, model = prepare_geo_evaluation_data(
+        submission, target, window, metric, month
+    )
     fig, ax, crs = setup_map_plot(target)
-    
+
     if metric_ticks is None:
         metric_ticks, drop_value = get_metric_ticks(metric, target)
         vmin, vmax = df["value"].min(), drop_value
         cbar = plt.cm.ScalarMappable(
-            norm=matplotlib.colors.SymLogNorm(linthresh=10, vmin=vmin, vmax=vmax, base=10),
+            norm=matplotlib.colors.SymLogNorm(
+                linthresh=10, vmin=vmin, vmax=vmax, base=10
+            ),
             cmap=cmap,
         )
-    
+
     num_countries_dropped = (df["value"] > drop_value).sum()
     df["value"] = np.where(df["value"] > drop_value, drop_value, df["value"])
-    
+
     sns.set_theme(style="white")
-    
+
     if target == "cm":
         df = df.to_crs(crs.proj4_init + " +over")
-        ax.add_geometries(df["geometry"], crs=crs, facecolor="none", edgecolor="whitesmoke")
+        ax.add_geometries(
+            df["geometry"], crs=crs, facecolor="none", edgecolor="whitesmoke"
+        )
         ax.set_global()
     else:
         cm_map = gpd.read_file(get_shapefile_path("cm"))
         df = df.to_crs(crs)
         cm_map.boundary.plot(ax=ax, linewidth=0.2)
         ax.set_extent([-20, 70, -40, 45])
-    
+
     if views_logo:
         add_views_logo(fig, target)
-    
+
     df.plot(
         ax=ax,
         column="value",
@@ -270,7 +308,7 @@ def choropleth_map_evaluation(
         edgecolor="#FF000000",
         cmap=cmap,
     )
-    
+
     fig.colorbar(
         cbar,
         ax=ax,
@@ -278,7 +316,7 @@ def choropleth_map_evaluation(
         label=metric.upper(),
         ticks=metric_ticks,
     )
-    
+
     if num_countries_dropped > 0:
         info_text = f"# countries truncated to {drop_value}: {num_countries_dropped}"
         if target == "pgm":
@@ -286,10 +324,10 @@ def choropleth_map_evaluation(
         ib_ax = fig.add_axes(info_box_placement)
         ib_ax.text(0, 0, info_text, ha="left", fontstyle="italic")
         ib_ax.axis("off")
-    
+
     ax.gridlines(draw_labels=True, zorder=0)
     ax.set_title(
-        format_title(team, model, window),
+        format_title(team, model, window, month),
         fontsize=16,
         pad=20,
         loc="left",
